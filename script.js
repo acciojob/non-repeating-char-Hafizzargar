e9b89e4778cf8449e25b9b394626a3c86365a22b
Hafizzargar <96607751+Hafizzargar@users.noreply.github.com>
2023-09-18 20:35:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+	let s="";
+ for(let i=0;i<str.length;i++){
+	 let x=str.charAt(i);
+	 let c=0;
+
+	 for(let j=0;j<str.length;j++){
+		 if(x===str.charAt(j)){
+			 c++;
+		 }
+	 }
+	 if(c==1){
+		 s=s+str.charAt(i);
+	 }
+ }
+ return s;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
